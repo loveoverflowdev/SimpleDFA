5120987396e827c4d789e5c1d16283f23f4e26f8
Asuraman <manhpdbiz@gmail.com>
2023-09-04 12:57:02 +0700

    initial commit

---
+++ src/main/java/org/asura/dfa/MathExpressionParser.java
@@ -0,0 +1,73 @@
+package org.asura.dfa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MathExpressionParser {
+
+    private enum State {
+        START, NUMBER, OPERATOR
+    }
+
+    public static List<Object> parseExpression(String expression) throws Exception {
+        List<Object> tokens = new ArrayList<>();
+        StringBuilder currentToken = new StringBuilder();
+        State currentState = State.START;
+
+        for (char c : expression.toCharArray()) {
+            switch (currentState) {
+                case START -> {
+                    if (Character.isDigit(c) || c == '.') {
+                        currentToken.append(c);
+                        currentState = State.NUMBER;
+                    } else if (isOperator(c)) {
+                        tokens.add(String.valueOf(c));
+                        currentState = State.OPERATOR;
+                    } else if (!Character.isWhitespace(c)) {
+                        throw new Exception("Invalid character: " + c);
+                    }
+                }
+                case NUMBER -> {
+                    if (Character.isDigit(c) || c == '.') {
+                        currentToken.append(c);
+                    } else if (isOperator(c)) {
+                        tokens.add(parseNumber(currentToken.toString()));
+                        tokens.add(String.valueOf(c));
+                        currentToken.setLength(0);
+                        currentState = State.START;
+                    } else if (!Character.isWhitespace(c)) {
+                        throw new Exception("Invalid character: " + c);
+                    }
+                }
+            }
+        }
+
+        if (currentState == State.NUMBER) {
+            tokens.add(parseNumber(currentToken.toString()));
+        }
+
+        return tokens;
+    }
+
+    private static boolean isOperator(char c) {
+        return c == '+' || c == '-' || c == '*' || c == '/';
+    }
+
+    private static Object parseNumber(String str) {
+        if (str.contains(".")) {
+            return Double.parseDouble(str);
+        } else {
+            return Integer.parseInt(str);
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            String expression = " 01+2*3.5-4 / 2";
+            List<Object> tokens = parseExpression(expression);
+            System.out.println(tokens);
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+}
